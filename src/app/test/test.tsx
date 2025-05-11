fb4214d61a4530031343702979e807785f14d925
ananth-krishna-123 <ananthakrishnan@hellomentor.in>
2025-05-11 13:24:49 +0530

    feat(test): log atomic commit line

---
+++ src/app/test/test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type Props = {}
+
+const test = (props: Props) => {
+    console.log("sample first line")
+
+    console.log("sample second line")
+
+    console.log("this is a new line for atomic commit")
+
+  return (
+    <div>test</div>
+  )
+}
+
+export default test
